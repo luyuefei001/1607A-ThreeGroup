56d7fb056d093258331ed21995e0fb630d8a1f85
卢跃飞001 <1404411437@qq.com>
2019-03-28 00:15:01 +0800

    商品品牌的完成

--- blog/resources/views/backend/shop/shopBrandUpdate.blade.php
+++ blog/resources/views/backend/shop/shopBrandUpdate.blade.php
@@ -10,49 +10,38 @@
 
 <h1>
 <span class="action-span"><a href="{{url('backend/shopBrand')}}">商品品牌</a></span>
-<span class="action-span1"><a href="{{url('backend/main')}}">SHOP 管理中心</a> </span><span id="search_id" class="action-span1"> - 添加品牌 </span>
+<span class="action-span1"><a href="{{url('backend/main')}}">SHOP 管理中心</a> </span><span id="search_id" class="action-span1"> - 修改品牌信息 </span>
 <div style="clear:both"></div>
 </h1>
 
 <div class="main-div">
-<form method="post" action="brand.php" name="theForm" enctype="multipart/form-data" onsubmit="return validate()">
+<form method="post" action=""  enctype="multipart/form-data" onsubmit="return validate()">
+@csrf
 <table cellspacing="1" cellpadding="3" width="100%">
+  <input type="hidden" name="shopBrand_id" value="{{$data[0]->shopBrand_id}}">
   <tbody><tr>
     <td class="label">品牌名称</td>
-    <td><input type="text" name="brand_name" maxlength="60" value=""><span class="require-field">*</span></td>
+    <td><input type="text" name="shopBrandName" maxlength="60" value="{{$data[0]->shopBrandName}}"><span class="require-field">*</span></td>
   </tr>
   <tr>
     <td class="label">品牌网址</td>
-    <td><input type="text" name="site_url" maxlength="60" size="40" value=""></td>
+    <td><input type="text" name="BrandUrl" maxlength="60" size="40" value="{{$data[0]->BrandUrl}}"></td>
   </tr>
   <tr>
     <td class="label"><a href="javascript:showNotice('warn_brandlogo');" title="点击此处查看提示信息">
-        <img src="images/notice.gif" width="16" height="16" border="0" alt="点击此处查看提示信息"></a>品牌LOGO</td>
+        <img src="{{asset($data[0]->logo)}}" width="50" height="50" border="0" alt="点击此处查看提示信息"></a>品牌LOGO</td>
     <td><input type="file" name="brand_logo" id="logo" size="45">    <br><span class="notice-span" style="display:block" id="warn_brandlogo">
-        请上传图片，做为品牌的LOGO！        </span>
+        如果修改请点击        </span>
     </td>
-  </tr>
-  <tr>
-    <td class="label">品牌描述</td>
-    <td><textarea name="brand_desc" cols="60" rows="4"></textarea></td>
   </tr>
   <tr>
     <td class="label">排序</td>
-    <td><input type="text" name="sort_order" maxlength="40" size="15" value="50"></td>
-  </tr>
-  <tr>
-    <td class="label">是否显示</td>
-    <td><input type="radio" name="is_show" value="1" checked="checked"> 是        <input type="radio" name="is_show" value="0"> 否        (当品牌下还没有商品的时候，首页及分类页的品牌区将不会显示该品牌。)
-    </td>
+    <td><input type="text" name="sort" maxlength="40" size="15" value="{{$data[0]->sort}}"></td>
   </tr>
   <tr>
     <td colspan="2" align="center"><br>
-      <input type="submit" class="button" value=" 确定 ">
+      <input type="submit" class="button" value=" 修改 ">
       <input type="reset" class="button" value=" 重置 ">
-      <input type="hidden" name="act" value="insert">
-      <input type="hidden" name="old_brandname" value="">
-      <input type="hidden" name="id" value="">
-      <input type="hidden" name="old_brandlogo" value="">
     </td>
   </tr>
 </tbody></table>
